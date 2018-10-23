1b540bd76302ad7d6df58922da9ed736985db185
Rob Doherty <robdohertykelly@gmail.com>
2018-10-23 18:12:59 +0100

    added Jenkinsfile_nightly to run tests overnight

--- e2e/ia.conf.js
+++ e2e/ia.conf.js
@@ -2,7 +2,7 @@ module.exports = {
 
   CcdGatewayUrl: process.env.TEST_E2E_URL_GATEWAY || 'https://gateway-ccd.nonprod.platform.hmcts.net',
   CcdWebUrl: process.env.TEST_E2E_URL_WEB || 'https://www-ccd.nonprod.platform.hmcts.net',
-  UseHeadlessBrowser: process.env.TEST_E2E_HEADLESS != 'false',
+  UseHeadlessBrowser: process.env.TEST_E2E_HEADLESS !== 'false',
   UseProxy: process.env.TEST_E2E_PROXY || 'http://proxyout.reform.hmcts.net:8080',
 
   TestCaseOfficerUserName: process.env.TEST_CASEOFFICER_USERNAME,
