31d6d76a309df55899285ed97b35d626fd87455a
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2018-10-31 12:21:59 +0000

    Scripted user creation and import process

--- README.md
+++ README.md
@@ -37,127 +37,13 @@ Follow the instructions here to get CCD running locally:
 
 https://github.com/hmcts/ccd-docker
 
-Once all of the docker containers are running, add the following user roles to the IDAM database:
+Once all of the docker containers are running, configure the environment variables shown below so that the tests run against the docker containers.
+
+Examples (the values below are not real, replace them with values *matching those in the latest CCD Definition spreadsheet*):
 
 ```
-psql -h 127.0.0.1 -p 5000 -U postgres -d idam -c \
-  "INSERT INTO role (name, display_name) VALUES
-     ('caseworker-ia', 'IA Case Worker'),
-     ('caseworker-ia-caseofficer', 'IA Case Officer'),
-     ('caseworker-ia-judiciary', 'IA Judiciary'),
-     ('caseworker-ia-legalrep-solicitor', 'IA Legal Rep'),
-     ('caseworker-ia-system', 'IA System')
-     ON CONFLICT DO NOTHING"
-```
+export CCD_DOCKER_PATH=/path/to/ccd-docker
 
-Then, create IDAM users *matching those in the latest CCD Definition*:
-
-Examples (these are not real users):
-
-```
-
-curl \
-  http://localhost:4501/testing-support/accounts \
-  -H "Content-Type: application/json" \
-  -d '{"email":"ia-caseofficer@example.com",
-       "forename":"Case",
-       "surname":"Officer",
-       "password":"password",
-       "levelOfAccess":1,
-       "roles":["caseworker-ia", "caseworker-ia-caseofficer"],
-       "userGroup": {"code": "caseworker"}
-      }'
-
-curl \
-  http://localhost:4501/testing-support/accounts \
-  -H "Content-Type: application/json" \
-  -d '{"email":"ia-judiciary@example.com",
-       "forename":"Tribunal",
-       "surname":"Judge",
-       "password":"password",
-       "levelOfAccess":1,
-       "roles":["caseworker-ia", "caseworker-ia-judiciary"],
-       "userGroup": {"code": "caseworker"}
-      }'
-
-curl \
-  http://localhost:4501/testing-support/accounts \
-  -H "Content-Type: application/json" \
-  -d '{"email":"ia-law-firm-a@example.com",
-       "forename":"LawFirm",
-       "surname":"A",
-       "password":"password",
-       "levelOfAccess":1,
-       "roles":["caseworker-ia", "caseworker-ia-legalrep-solicitor"],
-       "userGroup": {"code": "caseworker"}
-      }'
-
-curl \
-  http://localhost:4501/testing-support/accounts \
-  -H "Content-Type: application/json" \
-  -d '{"email":"ia-law-firm-b@example.com",
-       "forename":"LawFirm",
-       "surname":"B",
-       "password":"password",
-       "levelOfAccess":1,
-       "roles":["caseworker-ia", "caseworker-ia-legalrep-solicitor"],
-       "userGroup": {"code": "caseworker"}
-      }'
-```
-
-Add the roles into CCD:
-
-Examples (these are not real classifications):
-
-```
-export userToken="$($CCD_DOCKER_PATH/bin/idam-user-token.sh)"
-export serviceToken="$($CCD_DOCKER_PATH/bin/idam-service-token.sh)"
-
-curl --silent -XPUT \
-  http://localhost:4451/api/user-role \
-  -H "Authorization: Bearer ${userToken}" \
-  -H "ServiceAuthorization: Bearer ${serviceToken}" \
-  -H "Content-Type: application/json" \
-  -d '{"role":"'caseworker-ia-caseofficer'","security_classification":"'PUBLIC'"}'
-
-curl --silent -XPUT \
-  http://localhost:4451/api/user-role \
-  -H "Authorization: Bearer ${userToken}" \
-  -H "ServiceAuthorization: Bearer ${serviceToken}" \
-  -H "Content-Type: application/json" \
-  -d '{"role":"'caseworker-ia-judiciary'","security_classification":"'PUBLIC'"}'
-
-curl --silent -XPUT \
-  http://localhost:4451/api/user-role \
-  -H "Authorization: Bearer ${userToken}" \
-  -H "ServiceAuthorization: Bearer ${serviceToken}" \
-  -H "Content-Type: application/json" \
-  -d '{"role":"'caseworker-ia-legalrep-solicitor'","security_classification":"'PUBLIC'"}'
-
-curl --silent -XPUT \
-  http://localhost:4451/api/user-role \
-  -H "Authorization: Bearer ${userToken}" \
-  -H "ServiceAuthorization: Bearer ${serviceToken}" \
-  -H "Content-Type: application/json" \
-  -d '{"role":"'caseworker-ia-system'","security_classification":"'PUBLIC'"}'
-
-```
-
-Import a CCD definition spreadsheet:
-
-```
-curl --silent \
-  http://127.0.0.1:4451/import \
-  -H "Authorization: Bearer ${userToken}" \
-  -H "ServiceAuthorization: Bearer ${serviceToken}" \
-  -F file="@/path/to/definition/spreadsheet"
-```
-
-Configure the following environment variables so that the tests run against the docker containers:
-
-Examples (these are not real users):
-
-```
 export TEST_E2E_URL_GATEWAY=http://localhost:3453
 export TEST_E2E_URL_WEB=http://localhost:3451
 export TEST_E2E_USE_PROXY=false
@@ -174,6 +60,14 @@ export TEST_LAW_FIRM_A_PASSWORD=password
 export TEST_LAW_FIRM_B_USERNAME=ia-legal-rep-b@example.com
 export TEST_LAW_FIRM_B_PASSWORD=password
 ```
+
+Configure CCD with the users exported to your environment (do this once when you first run ccd-docker):
+
+```./bin/create-users.sh```
+
+Import the CCD Definition spreadsheet:
+
+```./bin/import-definition.sh /path/to/definition/spreadsheet```
 
 ## Other tasks
 
---
+++ bin/create-users.sh
@@ -0,0 +1,155 @@
+#!/usr/bin/env bash
+
+set -o nounset
+
+if [[ -z "${CCD_DOCKER_PATH}" ]]; then
+    echo "ERROR: CCD_DOCKER_PATH must be defined in the environment" 1>&2
+    exit 1
+fi
+
+if [[ -z "${TEST_CASEOFFICER_USERNAME}" ]]; then
+    echo "ERROR: TEST_CASEOFFICER_USERNAME must be defined in the environment" 1>&2
+    exit 1
+fi
+
+if [[ -z "${TEST_CASEOFFICER_PASSWORD}" ]]; then
+    echo "ERROR: TEST_CASEOFFICER_PASSWORD must be defined in the environment" 1>&2
+    exit 1
+fi
+
+if [[ -z "${TEST_JUDICIARY_USERNAME}" ]]; then
+    echo "ERROR: TEST_JUDICIARY_USERNAME must be defined in the environment" 1>&2
+    exit 1
+fi
+
+if [[ -z "${TEST_JUDICIARY_PASSWORD}" ]]; then
+    echo "ERROR: TEST_JUDICIARY_PASSWORD must be defined in the environment" 1>&2
+    exit 1
+fi
+
+if [[ -z "${TEST_LAW_FIRM_A_USERNAME}" ]]; then
+    echo "ERROR: TEST_LAW_FIRM_A_USERNAME must be defined in the environment" 1>&2
+    exit 1
+fi
+
+if [[ -z "${TEST_LAW_FIRM_A_PASSWORD}" ]]; then
+    echo "ERROR: TEST_LAW_FIRM_A_PASSWORD must be defined in the environment" 1>&2
+    exit 1
+fi
+
+if [[ -z "${TEST_LAW_FIRM_B_USERNAME}" ]]; then
+    echo "ERROR: TEST_LAW_FIRM_B_USERNAME must be defined in the environment" 1>&2
+    exit 1
+fi
+
+if [[ -z "${TEST_LAW_FIRM_B_PASSWORD}" ]]; then
+    echo "ERROR: TEST_LAW_FIRM_B_PASSWORD must be defined in the environment" 1>&2
+    exit 1
+fi
+
+CCD_DEFINITION_STORE_API_IS_RUNNING=false
+(exec 6<>/dev/tcp/127.0.0.1/4451) &>/dev/null && CCD_DEFINITION_STORE_API_IS_RUNNING=true || echo "ERROR: CCD Definition Store API is not running" 1>&2
+exec 6>&- # close output connection
+exec 6<&- # close input connection
+
+CCD_SHARED_DATABASE_IS_RUNNING=false
+(exec 6<>/dev/tcp/127.0.0.1/5000) &>/dev/null && CCD_SHARED_DATABASE_IS_RUNNING=true || echo "ERROR: CCD shared database docker container is not running" 1>&2
+exec 6>&- # close output connection
+exec 6<&- # close input connection
+
+IDAM_API_IS_RUNNING=false
+(exec 6<>/dev/tcp/127.0.0.1/4501) &>/dev/null && IDAM_API_IS_RUNNING=true || echo "ERROR: IDAM API docker container is not running" 1>&2
+exec 6>&- # close output connection
+exec 6<&- # close input connection
+
+${CCD_SHARED_DATABASE_IS_RUNNING} || exit 1
+${IDAM_API_IS_RUNNING} || exit 1
+${CCD_DEFINITION_STORE_API_IS_RUNNING} || exit 1
+
+USER_TOKEN="$(${CCD_DOCKER_PATH}/bin/idam-user-token.sh)"
+SERVICE_TOKEN="$(${CCD_DOCKER_PATH}/bin/idam-service-token.sh)"
+
+psql -h 127.0.0.1 -p 5000 -U postgres -d idam -c \
+  "INSERT INTO role (name, display_name) VALUES
+     ('caseworker-ia', 'IA Case Worker'),
+     ('caseworker-ia-caseofficer', 'IA Case Officer'),
+     ('caseworker-ia-judiciary', 'IA Judiciary'),
+     ('caseworker-ia-legalrep-solicitor', 'IA Legal Rep'),
+     ('caseworker-ia-system', 'IA System')
+     ON CONFLICT DO NOTHING"
+
+curl \
+  http://localhost:4501/testing-support/accounts \
+  -H "Content-Type: application/json" \
+  -d '{"email":"'"${TEST_CASEOFFICER_USERNAME}"'",
+       "forename":"Case",
+       "surname":"Officer",
+       "password":"'"${TEST_CASEOFFICER_PASSWORD}"'",
+       "levelOfAccess":1,
+       "roles":["caseworker-ia", "caseworker-ia-caseofficer"],
+       "userGroup": {"code": "caseworker"}
+      }'
+
+curl \
+  http://localhost:4501/testing-support/accounts \
+  -H "Content-Type: application/json" \
+  -d '{"email":"'"${TEST_JUDICIARY_USERNAME}"'",
+       "forename":"Tribunal",
+       "surname":"Judge",
+       "password":"'"${TEST_JUDICIARY_PASSWORD}"'",
+       "levelOfAccess":1,
+       "roles":["caseworker-ia", "caseworker-ia-judiciary"],
+       "userGroup": {"code": "caseworker"}
+      }'
+
+curl \
+  http://localhost:4501/testing-support/accounts \
+  -H "Content-Type: application/json" \
+  -d '{"email":"'"${TEST_LAW_FIRM_A_USERNAME}"'",
+       "forename":"LawFirm",
+       "surname":"A",
+       "password":"'"${TEST_LAW_FIRM_A_PASSWORD}"'",
+       "levelOfAccess":1,
+       "roles":["caseworker-ia", "caseworker-ia-legalrep-solicitor"],
+       "userGroup": {"code": "caseworker"}
+      }'
+
+curl \
+  http://localhost:4501/testing-support/accounts \
+  -H "Content-Type: application/json" \
+  -d '{"email":"'"${TEST_LAW_FIRM_B_USERNAME}"'",
+       "forename":"LawFirm",
+       "surname":"B",
+       "password":"'"${TEST_LAW_FIRM_B_PASSWORD}"'",
+       "levelOfAccess":1,
+       "roles":["caseworker-ia", "caseworker-ia-legalrep-solicitor"],
+       "userGroup": {"code": "caseworker"}
+      }'
+
+curl --silent -XPUT \
+  http://localhost:4451/api/user-role \
+  -H "Authorization: Bearer ${USER_TOKEN}" \
+  -H "ServiceAuthorization: Bearer ${SERVICE_TOKEN}" \
+  -H "Content-Type: application/json" \
+  -d '{"role":"caseworker-ia-caseofficer","security_classification":"PUBLIC"}'
+
+curl --silent -XPUT \
+  http://localhost:4451/api/user-role \
+  -H "Authorization: Bearer ${USER_TOKEN}" \
+  -H "ServiceAuthorization: Bearer ${SERVICE_TOKEN}" \
+  -H "Content-Type: application/json" \
+  -d '{"role":"caseworker-ia-judiciary","security_classification":"PUBLIC"}'
+
+curl --silent -XPUT \
+  http://localhost:4451/api/user-role \
+  -H "Authorization: Bearer ${USER_TOKEN}" \
+  -H "ServiceAuthorization: Bearer ${SERVICE_TOKEN}" \
+  -H "Content-Type: application/json" \
+  -d '{"role":"caseworker-ia-legalrep-solicitor","security_classification":"PUBLIC"}'
+
+curl --silent -XPUT \
+  http://localhost:4451/api/user-role \
+  -H "Authorization: Bearer ${USER_TOKEN}" \
+  -H "ServiceAuthorization: Bearer ${SERVICE_TOKEN}" \
+  -H "Content-Type: application/json" \
+  -d '{"role":"caseworker-ia-system","security_classification":"PUBLIC"}'
