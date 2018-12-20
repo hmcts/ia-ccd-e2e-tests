b6f53bae08934c48d79f8efedc1eeeb04818939d
Jamie Burns <1443443+fantasticjamieburns@users.noreply.github.com>
2018-12-20 16:30:56 +0000

    RIA-385 Request hearing requirements (#48)

--- e2e/features/stepDefinitions/service/respondent-response.steps.ts
+++ e2e/features/stepDefinitions/service/respondent-response.steps.ts
@@ -1,8 +1,14 @@
+import { AddAppealResponseFlow } from '../../../flows/add-appeal-response.flow';
 import { RequestRespondentReviewFlow } from '../../../flows/request-respondent-review.flow';
 import { Then } from 'cucumber';
 
+const addAppealResponseFlow = new AddAppealResponseFlow();
 const requestRespondentReviewFlow = new RequestRespondentReviewFlow();
 
 Then(/^I request respondent review$/, async function () {
     await requestRespondentReviewFlow.requestRespondentReview(true);
+});
+
+Then(/^I add the appeal response$/, async function () {
+    await addAppealResponseFlow.addAppealResponse(true);
 });
