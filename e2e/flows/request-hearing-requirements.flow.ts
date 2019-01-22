b0fe99e4cbb202476d1c455f412d1447a951875f
Tony Mortimer <mrmort80@gmail.com>
2019-01-22 09:22:14 +0000

    RIA-944: Testing hearing requirements progress images for overview tab

---
+++ e2e/flows/request-hearing-requirements.flow.ts
@@ -0,0 +1,14 @@
+import { CcdFormPage } from '../pages/ccd-form.page';
+
+export class RequestHearingRequirementsFlow {
+
+    private ccdFormPage = new CcdFormPage();
+
+    async requestHearingRequirements() {
+
+        await this.ccdFormPage.selectNextStep('Request hearing requirements');
+        await this.ccdFormPage.click('Go');
+
+        await this.ccdFormPage.click('Send direction');
+    }
+}
