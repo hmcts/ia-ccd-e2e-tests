6f50343494d234fa918f6849ac0404ed9131111c
Rob Doherty <robdohertykelly@gmail.com>
2019-02-13 11:31:20 +0000

    extended `normal` wait for AAT

--- e2e/enums/wait.ts
+++ e2e/enums/wait.ts
@@ -3,6 +3,6 @@ const iaConfig = require('../ia.conf');
 export enum Wait {
     minimal = iaConfig.RunningOnAAT ? 3000 : 100,
     short = iaConfig.RunningOnAAT ? 10000 : 1000,
-    normal = iaConfig.RunningOnAAT ? 30000 : 10000,
+    normal = iaConfig.RunningOnAAT ? 40000 : 10000,
     long = iaConfig.RunningOnAAT ? 120000 : 60000
 }
