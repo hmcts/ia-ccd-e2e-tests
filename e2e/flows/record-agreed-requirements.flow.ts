1c4e59d401568a366bbdf41479479477f435818d
jkumar-moj <54027687+jkumar-moj@users.noreply.github.com>
2020-02-28 15:52:54 +0000

    RIA-2398: Changes to relisting case after review update hearing requirements (#199)

--- e2e/flows/record-agreed-requirements.flow.ts
+++ e2e/flows/record-agreed-requirements.flow.ts
@@ -58,6 +58,62 @@ export class RecordAgreedRequirementsFlow {
         }
     }
 
+    async recordUpdatedHearingRequirementsYesPath(clickContinue = false) {
+
+        await this.ccdFormPage.selectNextStep('Update hearing adjustments');
+        await this.ccdFormPage.click('Go');
+
+        await this.ccdFormPage.setFieldValue(
+            'Length of hearing',
+            '6 hours'
+        );
+
+        await this.ccdFormPage.click('Continue');
+
+        await this.ccdFormPage.setFieldValue(
+            'Adjustments to accommodate vulnerabilities',
+            'Adjustments to accommodate vulnerabilities will be reviewed - updated'
+        );
+
+        await this.ccdFormPage.click('Continue');
+
+        await this.ccdFormPage.setFieldValue(
+            'Multimedia equipment',
+            'Multimedia equipment requirement will be reviewed - updated'
+        );
+
+        await this.ccdFormPage.click('Continue');
+
+        await this.ccdFormPage.setFieldValue(
+            'Single-sex court',
+            'Single sex court requirement will be reviewed - updated'
+        );
+
+        await this.ccdFormPage.click('Continue');
+
+        await this.ccdFormPage.setFieldValue(
+            'In camera court',
+            'Private hearing requirement will be reviewed - updated'
+        );
+
+        await this.ccdFormPage.click('Continue');
+
+        await this.ccdFormPage.setFieldValue(
+            'Other adjustments',
+            'Additional adjustments requirement will be reviewed - updated'
+        );
+
+        await this.ccdFormPage.click('Continue');
+
+        await this.ccdFormPage.headingContains('Check your answers');
+        await this.ccdFormPage.click('Update');
+
+        if (clickContinue) {
+            await this.ccdFormPage.click('Close and Return to case details');
+            await this.ccdFormPage.waitUntilLoaded();
+        }
+    }
+
     async recordAgreedRequirementsNoPath(clickContinue = false) {
 
         await this.ccdFormPage.selectNextStep('Hearing requirements');
