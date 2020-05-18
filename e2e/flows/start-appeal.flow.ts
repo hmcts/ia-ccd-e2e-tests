a57a23e945808bd2a1374070da2aff9194a7c1e4
David Crespo <david.crespo@hmcts.net>
2020-05-18 17:52:37 +0200

    RIA-3221 changes (#251)

--- e2e/flows/start-appeal.flow.ts
+++ e2e/flows/start-appeal.flow.ts
@@ -6,9 +6,10 @@ export class StartAppealFlow {
 
     async completeScreeningQuestions(clickContinue = false) {
 
-        await this.ccdFormPage.click('My client is not currently in detention');
-        await this.ccdFormPage.click('My client isn\'t appealing with other people as part of a family appeal');
-        await this.ccdFormPage.click('My client is presently in the UK');
+        await this.ccdFormPage.click('My client is living in the UK');
+        await this.ccdFormPage.click('My client is not in detention');
+        await this.ccdFormPage.click('My client is not appealing an EU Settlement Scheme decision');
+        await this.ccdFormPage.click('My client is not appealing with anyone else as part of a linked or grouped appeal');
 
         if (clickContinue) {
             await this.ccdFormPage.click('Continue');
