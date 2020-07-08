ff3abf589bc9107005dccf29a29868d6be25720b
David Crespo <david.crespo@hmcts.net>
2020-07-08 15:34:08 +0200

    Feature/ia 2918 hide save and continue (#234)

--- e2e/flows/submit-case.flow.ts
+++ e2e/flows/submit-case.flow.ts
@@ -1,4 +1,7 @@
 import { CcdFormPage } from '../pages/ccd-form.page';
+import { Logger } from '../helpers/logger';
+
+const isSaveAndContinueEnabled = require('../ia.conf').isSaveAndContinueEnabled === 'true';
 
 export class SubmitCaseFlow {
 
@@ -6,13 +9,17 @@ export class SubmitCaseFlow {
 
     async submitCase(clickContinue = false) {
 
+      if (isSaveAndContinueEnabled) {
         await this.ccdFormPage.selectNextStep('Submit your case');
         await this.ccdFormPage.click('Go');
 
         await this.ccdFormPage.click('Submit');
 
         if (clickContinue) {
-            await this.ccdFormPage.click('Close and Return to case details');
+          await this.ccdFormPage.click('Close and Return to case details');
         }
+      } else {
+        Logger.log(`INFO: 'Submit your case' next step flow is ignored because the Save and Continue feature is disabled`);
+      }
     }
 }
