0040f957a542ef706194504a2b246de4edea3330
Brad Sorour <48841470+bradsorour@users.noreply.github.com>
2020-09-22 16:26:16 +0100

    RIA-3434 pay offline by card (#295)

--- e2e/features/stepDefinitions/service/start-appeal.steps.ts
+++ e2e/features/stepDefinitions/service/start-appeal.steps.ts
@@ -94,6 +94,10 @@ Given(/^I save my initial `?([^\s`]+)`? appeal type `?([^\s`]+)`? hearing fee$/,
     await startAppealFlow.saveInitialAppealWithFee(true, appealType, feeType);
 });
 
+Given(/^I save my initial `?([^\s`]+)`? appeal type `?([^\s`]+)`? hearing fee and pay offline$/, async function (appealType, feeType) {
+    await startAppealFlow.saveInitialAppealWithFeePayOffline(true, appealType, feeType);
+});
+
 Given('I save my initial appeal with out of time decision letter', async function () {
     await startAppealFlow.saveOutOfTimeAppeal(true);
 });
