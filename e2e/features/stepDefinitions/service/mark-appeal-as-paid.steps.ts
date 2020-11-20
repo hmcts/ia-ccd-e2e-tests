323da0eaa31c35c06119d28f9c723d2190a51d99
tarun-palisetty <tarun.mca@gmail.com>
2020-11-20 14:32:52 +0000

    RIA-3792 CO and AO manage fee update

---
+++ e2e/features/stepDefinitions/service/mark-appeal-as-paid.steps.ts
@@ -0,0 +1,8 @@
+import { Then } from 'cucumber';
+import { MarkAppealAsPaidFlow } from '../../../flows/mark-appeal-as-paid.flow';
+
+const markAppealAsPaidFlow = new MarkAppealAsPaidFlow();
+
+Then(/^I mark the appeal as paid/, async function () {
+    await markAppealAsPaidFlow.markAppealAsPaid(true);
+});
