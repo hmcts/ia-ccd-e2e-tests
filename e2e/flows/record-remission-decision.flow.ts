8aa7763e3ac9f4a27d0cd6fce4a271ad496dbc38
tarun-palisetty <tarun.mca@gmail.com>
2020-12-10 23:47:47 +0000

    RIA-3218 Legal rep apply for a late remission

---
+++ e2e/flows/record-remission-decision.flow.ts
@@ -0,0 +1,34 @@
+import { CcdFormPage } from '../pages/ccd-form.page';
+
+export class RecordRemissionDecisionFlow {
+
+    private ccdFormPage = new CcdFormPage();
+
+    async partiallApproved(clickContinue = false) {
+
+        await this.ccdFormPage.selectNextStep('Record remission decision');
+        await this.ccdFormPage.click('Go');
+
+        await this.ccdFormPage.headingContains('Record remission decision');
+        await this.ccdFormPage.click('Partially approved');
+
+        await this.ccdFormPage.click('Continue');
+
+        await this.ccdFormPage.setFieldValue('Amount remitted', '100');
+        await this.ccdFormPage.setFieldValue('Amount left to pay', '40');
+        await this.ccdFormPage.setFieldValue(
+            'Reason for decision',
+            'This remission is partially approved',
+            'text area'
+        );
+
+        await this.ccdFormPage.click('Continue');
+
+        await this.ccdFormPage.headingContains('Check your answers');
+        await this.ccdFormPage.click('Record decision');
+
+        if (clickContinue) {
+            await this.ccdFormPage.click('Close and Return to case details');
+        }
+    }
+}
