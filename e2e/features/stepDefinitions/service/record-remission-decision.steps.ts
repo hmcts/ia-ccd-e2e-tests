bdb6db54f4da405677a41fe916291910125d3cbb
tarun-palisetty <tarun.mca@gmail.com>
2020-12-10 23:47:47 +0000

    RIA-3218 Legal rep apply for a late remission

---
+++ e2e/features/stepDefinitions/service/record-remission-decision.steps.ts
@@ -0,0 +1,8 @@
+import { Then } from 'cucumber';
+import { RecordRemissionDecisionFlow } from '../../../flows/record-remission-decision.flow';
+
+const recordRemissionDecisionFlow = new RecordRemissionDecisionFlow();
+
+Then(/^I record remission decision as partially approved/, async function () {
+    await recordRemissionDecisionFlow.partiallApproved(true);
+});
