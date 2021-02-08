d72d80c203f275801de871f9d819aebda7e1276f
mounikahmcts <mounika.ammineni1@hmcts.net>
2021-02-08 09:08:04 +0000

    update wait

--- e2e/enums/wait.ts
+++ e2e/enums/wait.ts
@@ -2,8 +2,8 @@ const iaConfig = require('../ia.conf');
 
 export enum Wait {
     instant = 1,
-    minimal = iaConfig.WaitForAngular ? 100 : 3000,
-    short = iaConfig.WaitForAngular ? 3000 : 10000,
+    minimal = iaConfig.WaitForAngular ? 1000 : 30000,
+    short = iaConfig.WaitForAngular ? 30000 : 100000,
     normal = iaConfig.WaitForAngular ? 15000 : 40000,
     long = iaConfig.WaitForAngular ? 30000 : 60000
 }
