ad15b76f1fbfccb6a55b394e21afc35341444890
rohith-tiru <rohith.tiru@hmcts.net>
2021-02-11 16:18:43 +0000

    Fix yarn.lock package version and set maxSauceLabs sessions to constant.

--- e2e/features.crossbrowser.conf.js
+++ e2e/features.crossbrowser.conf.js
@@ -38,7 +38,7 @@ const config = {
   allScriptsTimeout: 220000,
   useAllAngular2AppRoots: true,
   multiCapabilities: browserPlatformMatrix.multiCapabilities,
-  maxSessions: iaConfig.RunWithNumberOfBrowsers,
+  maxSessions: 4,
 
   plugins: [
     {
