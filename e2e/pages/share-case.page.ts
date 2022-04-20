17884ca1624d9f7732b5209f632a0297a3bda46a
nilay <nilaypatel913@hotmail.com>
2022-04-20 15:55:59 +0100

    select case tweak

--- e2e/pages/share-case.page.ts
+++ e2e/pages/share-case.page.ts
@@ -59,23 +59,8 @@ export class ShareCasePage {
         await lastCase.click();
     }
 
-    async selectAllCaseCheckbox(shortWait = false) {
+    async selectAllCaseCheckbox() {
         await browser.sleep(5000);
-        const checkboxPath = '//input[@id=\'select-all\']';
-
-        try {
-            await browser.wait(
-                async () => {
-                    return (await element
-                        .all(by.xpath(checkboxPath))
-                        .filter(e => e.isPresent())
-                        .count()) > 0;
-                },
-                shortWait ? Wait.minimal : Wait.normal
-            );
-        } catch (error) {
-            throw Error('Case list did not load in time...')
-        }
         await element(by.xpath('//input[@id=\'select-all\']')).click();
     }
 
