d57557acb47dac33c84a2bbaa21d281ba635dd17
nilay <nilaypatel913@hotmail.com>
2022-04-20 17:18:41 +0100

    test @s fixes

--- e2e/features/stepDefinitions/service/start-bail-application.steps.ts
+++ e2e/features/stepDefinitions/service/start-bail-application.steps.ts
@@ -1,9 +1,6 @@
-import { CcdFormPage } from '../../../pages/ccd-form.page';
 import { StartBailApplicationFlow } from '../../../flows/start-bail-application.flow';
 import { Given } from 'cucumber';
-import { expect } from 'chai';
 
-const ccdFormPage = new CcdFormPage();
 const startBailApplicationFlow = new StartBailApplicationFlow();
 
 Given(/^I save my initial application as a `(Admin Officer|Home Office|Legal Rep)` for a (Prison|Immigration removal centre) detention with (no|one|two) financial condition supporters and with (a|no) Legal Representative$/,
