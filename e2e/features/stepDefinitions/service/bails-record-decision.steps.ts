17239c461bce49838ae5f950d2aca20dbec099af
nilay <nilay.patel@hmcts.net>
2022-05-26 10:57:15 +0100

    testing background event

--- e2e/features/stepDefinitions/service/bails-record-decision.steps.ts
+++ e2e/features/stepDefinitions/service/bails-record-decision.steps.ts
@@ -3,18 +3,32 @@ import { Given } from 'cucumber';
 
 const recordDecision = new RecordDecision();
 
-// Given('I record a granted decision', async function () {
-//   await recordDecision.recordGrantedDecision(true);
-// });
-//
-// Given('I record a refused decision', async function () {
-//   await recordDecision.recordRefusedDecision(true);
-// });
-//
-// Given('I record a conditional grant decision', async function () {
-//   await recordDecision.recordConditionalGrantDecision(true);
-// });
-
-Given('I complete Conditions page', async function () {
-  await recordDecision.completeConditions(true, ['Appearance', 'Other']);
+Given('I record a refused decision with SS consent No', async function () {
+  await recordDecision.recordDecision(true, 'No', 'Refused', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A');
 });
+
+// Given('I record a refused decision with SS consent Yes', async function () {
+//   await recordDecision.recordDecision(true, 'SsConsentYesOrNo', 'tribunalDecision', 'SsConsentDecisionYesOrNo', 'releaseStatusYesOrNo', 'conditions[]', 'financialConditionYesOrNo', 'financialConditionDetailsYesOrNo', 'numberOfSupporters', 'yesOrNoForEachSupporter[]', 'bailTransferYesOrNo');
+// });
+//
+// Given('I record a refused decision with SS consent Yes and Minded to grant', async function () {
+//   await recordDecision.recordDecision(true);
+// });
+//
+// Given('I record a granted decision with SS consent No', async function () {
+//   await recordDecision.recordDecision(true);
+// });
+//
+// Given('I record a granted decision with SS consent Yes', async function () {
+//   await recordDecision.recordDecision(true);
+// });
+//
+// Given('I record a conditional grant decision with SS consent No', async function () {
+//   await recordDecision.recordDecision(true);
+// });
+//
+// Given('I record a conditional grant decision with SS consent Yes', async function () {
+//   await recordDecision.recordDecision(true);
+// });
+//
+// await recordDecision.recordDecision(true, 'SsConsentYesOrNo', 'tribunalDecision', 'SsConsentDecisionYesOrNo', 'releaseStatusYesOrNo', 'conditions[]', 'financialConditionYesOrNo', 'financialConditionDetailsYesOrNo', 'numberOfSupporters', 'yesOrNoForEachSupporter[]', 'bailTransferYesOrNo');
