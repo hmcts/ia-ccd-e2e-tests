576399c905a1302650ee874bede08cfec7e26224
nilay <nilay.patel@hmcts.net>
2022-06-24 14:56:05 +0100

    edit application postSubmit after submitting done

--- e2e/features/stepDefinitions/service/bails-edit-bail-application-post-submit.steps.ts
+++ e2e/features/stepDefinitions/service/bails-edit-bail-application-post-submit.steps.ts
@@ -3,7 +3,7 @@ import { Given } from 'cucumber';
 
 const editBailApplicationPostSubmitFlow = new EditBailApplicationPostSubmitFlow();
 
-Given(/^I edit my submitted application as a `(Admin Officer|Home Office Bails|Legal Rep)` for a (Prison|Immigration removal centre) detention with (no|one|two) financial condition supporters and with (a|no) Legal Representative$/,
+Given(/^I edit my submitted application as a `Admin Officer` for a (Prison|Immigration removal centre) detention with (no|one|two) financial condition supporters and with (a|no) Legal Representative$/,
     async function (
     user,
     detentionFacility,
