831ddabb4630df0c01b9ef89cc90edc8fe13bc16
nilay <nilaypatel913@hotmail.com>
2022-06-27 10:51:46 +0100

    report test

--- e2e/features.crossbrowser.conf.js
+++ e2e/features.crossbrowser.conf.js
@@ -42,7 +42,7 @@ const config = {
 
   plugins: [
     {
-      package: 'protractor-simple-cucumber-html-reporter-plugin',
+      package: 'protractor-multiple-cucumber-html-reporter-plugin',
       options: {
         automaticallyGenerateReport: true,
         removeExistingJsonReportFile: true,
