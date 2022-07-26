29767c58c1920aecb559fa9aba1df2b97c95e048
nilay <nilaypatel913@hotmail.com>
2022-07-26 17:20:54 +0100

    fix

--- e2e/features/stepDefinitions/service/respondent-evidence.steps.ts
+++ e2e/features/stepDefinitions/service/respondent-evidence.steps.ts
@@ -1,17 +1,11 @@
 import { RequestRespondentEvidenceFlow } from '../../../flows/request-respondent-evidence.flow';
 import { UploadAdditionalEvidenceFlow } from '../../../flows/upload-additional-evidence.flow';
 import { UploadRespondentEvidenceFlow } from '../../../flows/upload-respondent-evidence.flow';
-import { RequestHomeOfficeDataFlow } from '../../../flows/request-home-office-data.flow';
 import { Then } from 'cucumber';
 
-const requestHomeOfficeDataFlow = new RequestHomeOfficeDataFlow();
 const requestRespondentEvidenceFlow = new RequestRespondentEvidenceFlow();
 const uploadAdditionalEvidenceFlow = new UploadAdditionalEvidenceFlow();
 const uploadRespondentEvidenceFlow = new UploadRespondentEvidenceFlow();
-
-Then(/^I request home office data$/, async function () {
-    await requestHomeOfficeDataFlow.requestHomeOfficeData(true);
-});
 
 Then(/^I request respondent evidence$/, async function () {
     await requestRespondentEvidenceFlow.requestRespondentEvidence(true);
