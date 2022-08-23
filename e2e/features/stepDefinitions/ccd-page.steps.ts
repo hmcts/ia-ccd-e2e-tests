3f2373134c2ecf22fbdbd4fddabab1b257b3e28d
nilay <nilay.patel@hmcts.net>
2022-08-23 10:32:41 +0100

    small change

--- e2e/features/stepDefinitions/ccd-page.steps.ts
+++ e2e/features/stepDefinitions/ccd-page.steps.ts
@@ -24,17 +24,12 @@ Given('I create a new case', async function () {
     await ccdFormPage.setFieldValue('Jurisdiction', 'Immigration & Asylum');
     await ccdPage.doesDropdownHaveValues('Case type');
 
-    if (iaConfig.CcdWebUrl.includes('demo') ) {
-        await ccdFormPage.setFieldValue(
-            'Case type',
-            'Appeal* RIA-5126'
-        );
-    } else {
+//     if (iaConfig.CcdWebUrl.includes('aat') ) {
         await ccdFormPage.setFieldValue(
             'Case type',
             'Appeal* master'
         );
-    }
+//     }
 
     await ccdPage.doesDropdownHaveValues('Event');
     await ccdPage.isButtonEnabled('Start');
@@ -76,17 +71,12 @@ Given('I Apply case list filter', async function () {
     await ccdPage.doesDropdownHaveValues('Jurisdiction');
     await ccdFormPage.setFieldValue('Jurisdiction', 'Immigration & Asylum');
     await ccdPage.doesDropdownHaveValues('Case type');
-    if (iaConfig.CcdWebUrl.includes('demo') ) {
-        await ccdFormPage.setFieldValue(
-            'Case type',
-            'Appeal* RIA-5126'
-        );
-    } else {
+//     if (iaConfig.CcdWebUrl.includes('aat') ) {
         await ccdFormPage.setFieldValue(
             'Case type',
             'Appeal* master'
         );
-    }
+//     }
 
     await ccdPage.doesDropdownHaveValues('State');
     await ccdFormPage.setFieldValue(
