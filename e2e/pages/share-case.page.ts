2716cb3cd40bd1d40d562449ee3c2b9a4a64867b
Nilay <nilay.Patel@hmcts.net>
2022-08-25 12:55:29 +0100

    Fees and payments (#597)
    * Revert "Revert "Revert "Revert "Fees and payments test branch"" (#593)" (#594)"
    
    This reverts commit 8b2c848e227f15c42f9c7a97479e156167590fe1.
    
    * fix

--- e2e/pages/share-case.page.ts
+++ e2e/pages/share-case.page.ts
@@ -258,7 +258,7 @@ export class ShareCasePage {
         if (iaConfig.CcdWebUrl.includes('demo') ) {
             await ccdFormPage.setFieldValue(
                 'Case type',
-                'Appeal* aip-other-rele'
+                'Appeal* ia-ccd-definit'
             );
         }
         // await ccdFormPage.setFieldValue('Case type', 'Appeal* master');
