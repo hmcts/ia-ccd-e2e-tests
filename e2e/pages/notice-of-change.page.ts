14deb71f172091ae8d79e00c35291f3877a4b950
Nilay <nilay.Patel@hmcts.net>
2022-08-26 09:55:20 +0100

    Revert "Fees and payments (#599)" (#600)
    This reverts commit 1705db106b110be51cc8f16ff4cb4c01218c7fe9.

--- e2e/pages/notice-of-change.page.ts
+++ e2e/pages/notice-of-change.page.ts
@@ -96,7 +96,7 @@ export class NoticeOfChangePage {
     async getLatestCaseIdFromUrl(shortWait = false) {
         let url = await browser.driver.getCurrentUrl();
         let startIndex = url.indexOf('case-details/')
-        let endIndex = url.indexOf('#')
+        let endIndex = url.indexOf('/trigger')
         let caseId;
         if (url.includes('/trigger')) {
             caseId = url.substring(startIndex + 13, endIndex)
