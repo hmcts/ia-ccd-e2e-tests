687213fed75e8dddc4154128dd0b597b6cc19f89
nilay <nilay.patel@hmcts.net>
2022-09-15 11:50:35 +0100

    added case note retaining test

---
+++ e2e/features/stepDefinitions/service/bails-add-a-case-note.steps.ts
@@ -0,0 +1,12 @@
+import { BailsAddCaseNoteFlow } from '../../../flows/bails-add-a-case-note.flow';
+import { Given } from 'cucumber';
+
+const bailsAddCaseNoteFlow = new BailsAddCaseNoteFlow();
+
+Given('I add a case note', async function () {
+    await bailsAddCaseNoteFlow.addCaseNote(true, '');
+});
+
+Given(/^I add a case note for application `?([^`]+)`?$/, async function (applicationNumber) {
+    await bailsAddCaseNoteFlow.addCaseNote(true, applicationNumber);
+});
