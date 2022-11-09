2c37fca94abb8aa0e8dbc0a7cd123ad269452827
srihari <srihari.nakkalar@clarks.com>
2022-11-09 15:32:54 +0000

    reverted appeal type to master

--- e2e/features/stepDefinitions/ccd-page.steps.ts
+++ e2e/features/stepDefinitions/ccd-page.steps.ts
@@ -27,12 +27,12 @@ Given('I create a new case', async function () {
     if (iaConfig.CcdWebUrl.includes('aat') || iaConfig.CcdWebUrl.includes('pr')) {
         await ccdFormPage.setFieldValue(
             'Case type',
-            'Appeal* aip-noc-releas'
+            'Appeal* master'
         );
     } else if (iaConfig.CcdWebUrl.includes('demo')) {
         await ccdFormPage.setFieldValue(
             'Case type',
-            'Appeal* aip-noc-releas'
+            'Appeal* master'
         );
     }
 
@@ -79,7 +79,7 @@ Given('I Apply case list filter', async function () {
 //     if (iaConfig.CcdWebUrl.includes('aat') ) {
         await ccdFormPage.setFieldValue(
             'Case type',
-            'Appeal* aip-noc-releas'
+            'Appeal* master'
         );
 //     }
     await ccdPage.doesDropdownHaveValues('State');
