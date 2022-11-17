cd53ca62ce60f9bd318d4a156c6ba8679e698cf4
nilay <nilay.patel@hmcts.net>
2022-11-17 13:42:23 +0000

    nightly dev demo

--- e2e/pages/share-case.page.ts
+++ e2e/pages/share-case.page.ts
@@ -258,7 +258,7 @@ export class ShareCasePage {
         if (iaConfig.CcdWebUrl.includes('demo') ) {
             await ccdFormPage.setFieldValue(
                 'Case type',
-                'Appeal* aip-noc-releas'
+                'Appeal* ia-ccd-definit'
             );
         }
         // await ccdFormPage.setFieldValue('Case type', 'Appeal* master');
