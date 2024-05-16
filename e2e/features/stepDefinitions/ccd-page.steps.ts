c92c4f9024c2516212c1fae6d0e308f22f6595a1
rtiru-hmcts2 <rohith.tiru2@hmcts.net>
2024-05-16 16:34:45 +0100

    changed subtype back to appeal master.

--- e2e/features/stepDefinitions/ccd-page.steps.ts
+++ e2e/features/stepDefinitions/ccd-page.steps.ts
@@ -34,13 +34,13 @@ Given('I create a new case', async function () {
   await ccdPage.doesDropdownHaveValues('Case type');
 
   if (iaConfig.CcdWebUrl.includes('xui-ia-case-api-pr')) {
-    await ccdFormPage.setFieldValue('Case type', 'Appeal* ia-ccd-definit');
+    await ccdFormPage.setFieldValue('Case type', 'Appeal* master');
   } else if (iaConfig.CcdWebUrl.includes('aat') || iaConfig.CcdWebUrl.includes('pr')) {
-    await ccdFormPage.setFieldValue('Case type', 'Appeal* ia-ccd-definit');
+    await ccdFormPage.setFieldValue('Case type', 'Appeal* master');
   } else if (iaConfig.CcdWebUrl.includes('demo')) {
-    await ccdFormPage.setFieldValue('Case type', 'Appeal* ia-ccd-definit');
+    await ccdFormPage.setFieldValue('Case type', 'Appeal* master');
   } else if (iaConfig.CcdWebUrl.includes('pr-')) {
-    await ccdFormPage.setFieldValue('Case type', 'Appeal* ia-ccd-definit');
+    await ccdFormPage.setFieldValue('Case type', 'Appeal* master');
   }
 
   await ccdPage.doesDropdownHaveValues('Event');
@@ -92,9 +92,9 @@ Given('I Apply case list filter', async function () {
   await ccdFormPage.setFieldValue('Jurisdiction', 'Immigration & Asylum');
   await ccdPage.doesDropdownHaveValues('Case type');
   if (iaConfig.CcdWebUrl.includes('aat') || iaConfig.CcdWebUrl.includes('pr')) {
-    await ccdFormPage.setFieldValue('Case type', 'Appeal* ia-ccd-definit');
+    await ccdFormPage.setFieldValue('Case type', 'Appeal* master');
   } else if (iaConfig.CcdWebUrl.includes('demo')) {
-    await ccdFormPage.setFieldValue('Case type', 'Appeal* ia-ccd-definit');
+    await ccdFormPage.setFieldValue('Case type', 'Appeal* master');
   }
   await ccdPage.doesDropdownHaveValues('State');
   await ccdFormPage.setFieldValue('State', 'Any');
