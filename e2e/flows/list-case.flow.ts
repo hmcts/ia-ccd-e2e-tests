80acbdaf7292a85128cced0bcb525199cc473c84
PrasadKumarP <prasad.kumar@hmcts.net>
2024-08-14 16:46:55 +0100

    updated in list case file

--- e2e/flows/list-case.flow.ts
+++ e2e/flows/list-case.flow.ts
@@ -1,156 +1,21 @@
-import { CcdFormPage } from "../pages/ccd-form.page";
-import { browser } from "protractor";
-import { by, element } from "protractor";
+import { CcdFormPage } from '../pages/ccd-form.page';
+import { browser } from 'protractor';
+import { by, element } from 'protractor';
 
 export class ListCaseFlow {
   private ccdFormPage = new CcdFormPage();
 
   async listTheCase(clickContinue = false) {
-    await this.ccdFormPage.selectNextStep("List the case");
+    await this.ccdFormPage.selectNextStep('List the case');
     let overviewUrl = await browser.getCurrentUrl();
-    await this.ccdFormPage.flakeyClick("Go", overviewUrl);
+    await this.ccdFormPage.flakeyClick('Go', overviewUrl);
     await this.ccdFormPage.waitForSpinner();
 
-    await this.ccdFormPage.setFieldValue("Listing reference", "LP/12345/2019");
-    await this.ccdFormPage.setFieldValue("Location", "Taylor House");
+    await this.ccdFormPage.setFieldValue('Listing reference', 'LP/12345/2019');
+    await this.ccdFormPage.setFieldValue('Location', 'Taylor House');
 
-    await this.ccdFormPage.setFieldValue("Length", "6 hours");
+    await this.ccdFormPage.setFieldValue('Length', '6 hours');
 
-    await this.ccdFormPage.setFieldValue(
-      "Date and time",
-      "{$TODAY+14|DD-MM-YYYY} 10:30:00"
-    );
-
-    await this.ccdFormPage.click("Continue");
-    await this.ccdFormPage.click("List case");
-
-    if (clickContinue) {
-      await this.ccdFormPage.click("Close and Return to case details");
-    }
-  }
-
-  async listTheCaseAsRemoteHearing(clickContinue = false) {
-    await this.ccdFormPage.selectNextStep("List the case");
-    let overviewUrl = await browser.getCurrentUrl();
-    await this.ccdFormPage.flakeyClick("Go", overviewUrl);
-    await this.ccdFormPage.waitForSpinner();
-
-    await this.ccdFormPage.setFieldValue("Listing reference", "LP/12345/2019");
-    await this.ccdFormPage.setFieldValue("Location", "Remote hearing");
-
-    await this.ccdFormPage.setFieldValue("Length", "6 hours");
-
-    await this.ccdFormPage.setFieldValue(
-      "Date and time",
-      "{$TODAY+14|DD-MM-YYYY} 10:30:00"
-    );
-
-    await this.ccdFormPage.click("Continue");
-    await this.ccdFormPage.click("List case");
-
-    if (clickContinue) {
-      await this.ccdFormPage.click("Close and Return to case details");
-    }
-  }
-
-  async reListTheCase(clickContinue = false, lengthOfHearingInHours = "6") {
-    await this.ccdFormPage.selectNextStep("Edit case listing");
-    let overviewUrl = await browser.getCurrentUrl();
-    await this.ccdFormPage.flakeyClick("Go", overviewUrl);
-    await this.ccdFormPage.waitForSpinner();
-
-    await this.ccdFormPage.headingContains("Edit case listing");
-    await this.ccdFormPage.setFieldValue("Location", "Taylor House");
-
-    await this.ccdFormPage.setFieldValue(
-      "Length",
-      lengthOfHearingInHours.concat(" hours")
-    );
-
-    await this.ccdFormPage.setFieldValue(
-      "Date and time",
-      "{$TODAY+14|DD-MM-YYYY} 10:30:00"
-    );
-
-    await this.ccdFormPage.click("Continue");
-    await this.ccdFormPage.click("List case");
-
-    if (clickContinue) {
-      await this.ccdFormPage.click("Close and Return to case details");
-      await this.ccdFormPage.contentContains("You have listed the case");
-    }
-  }
-
-  async reListTheCaseAsRemoteHearing(
-    clickContinue = false,
-    lengthOfHearingInHours = "3"
-  ) {
-    await this.ccdFormPage.selectNextStep("Edit case listing");
-    let overviewUrl = await browser.getCurrentUrl();
-    await this.ccdFormPage.flakeyClick("Go", overviewUrl);
-    await this.ccdFormPage.waitForSpinner();
-
-    await this.ccdFormPage.headingContains("Edit case listing");
-    await this.ccdFormPage.setFieldValue("Location", "Remote hearing");
-
-    await this.ccdFormPage.setFieldValue(
-      "Length",
-      lengthOfHearingInHours.concat(" hours")
-    );
-
-    await this.ccdFormPage.setFieldValue(
-      "Date and time",
-      "{$TODAY+16|DD-MM-YYYY} 11:45:00"
-    );
-
-    await this.ccdFormPage.click("Continue");
-    await this.ccdFormPage.click("List case");
-
-    if (clickContinue) {
-      await this.ccdFormPage.click("Close and Return to case details");
-      await this.ccdFormPage.contentContains("You have listed the case");
-    }
-  }
-
-  async reListTheCaseAsRemoteHearingWithNoChanges(
-    clickContinue = false,
-    lengthOfHearingInHours = "6"
-  ) {
-    await this.ccdFormPage.selectNextStep("Edit case listing");
-    let overviewUrl = await browser.getCurrentUrl();
-    await this.ccdFormPage.flakeyClick("Go", overviewUrl);
-    await this.ccdFormPage.waitForSpinner();
-
-    await this.ccdFormPage.headingContains("Edit case listing");
-    await this.ccdFormPage.setFieldValue("Location", "Remote hearing");
-
-    await this.ccdFormPage.setFieldValue(
-      "Length",
-      lengthOfHearingInHours.concat(" hours")
-    );
-
-    await this.ccdFormPage.setFieldValue(
-      "Date and time",
-      "{$TODAY+14|DD-MM-YYYY} 10:30:00"
-    );
-
-    await this.ccdFormPage.click("Continue");
-    await this.ccdFormPage.click("List case");
-
-    if (clickContinue) {
-      await this.ccdFormPage.click('Close and Return to case details');
-      await this.ccdFormPage.contentContains('You have listed the case');
-    }
-  }
-
-  async listTheBailCase(clickContinue = false) {
-    await this.ccdFormPage.selectNextStep('Case listing');
-    await this.ccdFormPage.click('Go');
-
-    await this.ccdFormPage.setFieldValue('Listing event', 'Initial listing');
-    await this.ccdFormPage.setFieldValue('Location', 'Hatton Cross');
-
-    await this.ccdFormPage.setFieldValue('Length', '1 hour');
     await this.ccdFormPage.setFieldValue('Date and time', '{$TODAY+14|DD-MM-YYYY} 10:30:00');
 
     await this.ccdFormPage.click('Continue');
@@ -161,17 +26,18 @@ export class ListCaseFlow {
     }
   }
 
-  // reListTheBailCase
-  async reListTheBailCase(clickContinue = false) {
-    await this.ccdFormPage.selectNextStep('Case listing');
-    await this.ccdFormPage.click('Go');
+  async listTheCaseAsRemoteHearing(clickContinue = false) {
+    await this.ccdFormPage.selectNextStep('List the case');
+    let overviewUrl = await browser.getCurrentUrl();
+    await this.ccdFormPage.flakeyClick('Go', overviewUrl);
+    await this.ccdFormPage.waitForSpinner();
 
-    await this.ccdFormPage.setFieldValue('Listing event', 'Relisting');
-    await this.ccdFormPage.setFieldValue('Location', 'Hatton Cross');
+    await this.ccdFormPage.setFieldValue('Listing reference', 'LP/12345/2019');
+    await this.ccdFormPage.setFieldValue('Location', 'Remote hearing');
 
-    await this.ccdFormPage.setFieldValue('Length', '30 minutes');
+    await this.ccdFormPage.setFieldValue('Length', '6 hours');
 
-    await this.ccdFormPage.setFieldValue('Date and time', '{$TODAY+14|DD-MM-YYYY} 11:30:00');
+    await this.ccdFormPage.setFieldValue('Date and time', '{$TODAY+14|DD-MM-YYYY} 10:30:00');
 
     await this.ccdFormPage.click('Continue');
     await this.ccdFormPage.click('List case');
@@ -180,4 +46,112 @@ export class ListCaseFlow {
       await this.ccdFormPage.click('Close and Return to case details');
     }
   }
+
+  async reListTheCase(clickContinue = false, lengthOfHearingInHours = '6') {
+    await this.ccdFormPage.selectNextStep('Edit case listing');
+    let overviewUrl = await browser.getCurrentUrl();
+    await this.ccdFormPage.flakeyClick('Go', overviewUrl);
+    await this.ccdFormPage.waitForSpinner();
+
+    await this.ccdFormPage.headingContains('Edit case listing');
+    await this.ccdFormPage.setFieldValue('Location', 'Taylor House');
+
+    await this.ccdFormPage.setFieldValue('Length', lengthOfHearingInHours.concat(' hours'));
+
+    await this.ccdFormPage.setFieldValue('Date and time', '{$TODAY+14|DD-MM-YYYY} 10:30:00');
+
+    await this.ccdFormPage.click('Continue');
+    await this.ccdFormPage.click('List case');
+
+    if (clickContinue) {
+      await this.ccdFormPage.click('Close and Return to case details');
+      await this.ccdFormPage.contentContains('You have listed the case');
+    }
+  }
+
+  async reListTheCaseAsRemoteHearing(clickContinue = false, lengthOfHearingInHours = '3') {
+    await this.ccdFormPage.selectNextStep('Edit case listing');
+    let overviewUrl = await browser.getCurrentUrl();
+    await this.ccdFormPage.flakeyClick('Go', overviewUrl);
+    await this.ccdFormPage.waitForSpinner();
+
+    await this.ccdFormPage.headingContains('Edit case listing');
+    await this.ccdFormPage.setFieldValue('Location', 'Remote hearing');
+
+    await this.ccdFormPage.setFieldValue('Length', lengthOfHearingInHours.concat(' hours'));
+
+    await this.ccdFormPage.setFieldValue('Date and time', '{$TODAY+16|DD-MM-YYYY} 11:45:00');
+
+    await this.ccdFormPage.click('Continue');
+    await this.ccdFormPage.click('List case');
+
+    if (clickContinue) {
+      await this.ccdFormPage.click('Close and Return to case details');
+      await this.ccdFormPage.contentContains('You have listed the case');
+    }
+  }
+
+  async reListTheCaseAsRemoteHearingWithNoChanges(clickContinue = false, lengthOfHearingInHours = '6') {
+    await this.ccdFormPage.selectNextStep('Edit case listing');
+    let overviewUrl = await browser.getCurrentUrl();
+    await this.ccdFormPage.flakeyClick('Go', overviewUrl);
+    await this.ccdFormPage.waitForSpinner();
+
+    await this.ccdFormPage.headingContains('Edit case listing');
+    await this.ccdFormPage.setFieldValue('Location', 'Remote hearing');
+
+    await this.ccdFormPage.setFieldValue('Length', lengthOfHearingInHours.concat(' hours'));
+
+    await this.ccdFormPage.setFieldValue('Date and time', '{$TODAY+14|DD-MM-YYYY} 10:30:00');
+
+    await this.ccdFormPage.click('Continue');
+    await this.ccdFormPage.click('List case');
+
+        if (clickContinue) {
+            await this.ccdFormPage.click('Close and Return to case details');
+            await this.ccdFormPage.contentContains('You have listed the case');
+        }
+    }
+
+    async listTheBailCase(clickContinue = false) {
+
+        await this.ccdFormPage.selectNextStep('Case listing');
+        await this.ccdFormPage.click('Go');
+        await this.ccdFormPage.setFieldValue('Listing event', 'Initial listing');
+        await this.ccdFormPage.setFieldValue('Listing location', 'Hatton Cross Tribunal Hearing Centre');
+        await this.ccdFormPage.setFieldValue('Will the hearing be held remotely?', 'Yes');
+        await this.ccdFormPage.typeText('listingHearingDuration', '60');
+        await this.ccdFormPage.setFieldValue(
+            'Date and time',
+            '{$TODAY+14|DD-MM-YYYY} 10:30:00'
+        );
+        await this.ccdFormPage.click('Continue');
+        await this.ccdFormPage.click('List case');
+        if (clickContinue) {
+            await this.ccdFormPage.click('Close and Return to case details');
+        }
+    }
+
+    async reListTheBailCase(clickContinue = false) {
+
+        await this.ccdFormPage.selectNextStep('Case listing');
+        await this.ccdFormPage.click('Go');
+        await this.ccdFormPage.setFieldValue('Listing event', 'Relisting');
+        await this.ccdFormPage.setFieldValue('Listing location', 'Hatton Cross Tribunal Hearing Centre');
+        await this.ccdFormPage.setFieldValue('Will the hearing be held remotely?', 'Yes');
+        await browser.sleep(3000);
+      let listingHearingDuaration = element(by.xpath("//*[@id='listingHearingDuration']"));
+       await listingHearingDuaration.clear();
+       await this.ccdFormPage.typeText('listingHearingDuration', '30');
+        await this.ccdFormPage.setFieldValue(
+            'Date and time',
+            '{$TODAY+14|DD-MM-YYYY} 11:30:00'
+        );
+        await this.ccdFormPage.click('Continue');
+        await this.ccdFormPage.click('List case');
+
+        if (clickContinue) {
+            await this.ccdFormPage.click('Close and Return to case details');
+        }
+    }
 }
