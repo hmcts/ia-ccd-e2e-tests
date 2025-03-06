39c69a00f6bb92132bac9f9e358ca76f32ea6758
sunil-vooturi <104900742+sunil-vooturi@users.noreply.github.com>
2025-03-06 10:00:57 +0000

    Update create-case-summary.flow.ts

--- e2e/flows/create-case-summary.flow.ts
+++ e2e/flows/create-case-summary.flow.ts
@@ -13,7 +13,7 @@ export class CaseSummaryFlow {
     await this.ccdFormPage.uploadFile('CaseSummary.pdf');
     await this.ccdFormPage.setFieldValue('Describe the document (Optional)', 'This is the case summary', 'text area'); 
     await this.ccdFormPage.click('Continue');
-    // await this.ccdFormPage.click('Upload');
+    await this.ccdFormPage.click('Upload');
 
     if (clickContinue) {
       await this.ccdFormPage.click('Close and Return to case details');
