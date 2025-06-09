638fc90ec6d324b16233b994a66f5056b8068cbf
nilay <nilay.patel@solirius.com>
2025-06-09 11:34:28 +0100

    fix

--- e2e/flows/pay-and-submit-appeal.flow.ts
+++ e2e/flows/pay-and-submit-appeal.flow.ts
@@ -7,7 +7,7 @@ export class PayAndSubmitAppealFlow {
   private ccdFormPage = new CcdFormPage();
 
   async payForAppealByPBA(clickContinue = false) {
-    await element(by.xpath('//div.mat-tab-label[text()="Service Request"]')).click();
+    await element(by.xpath('//div[text()="Service Request"][contains(@class, "mat-tab-label-content")]')).click();
     await this.ccdPage.waitForCssElementVisible('td.govuk-table__cell > a.ng-star-inserted');
     await this.ccdFormPage.click('Pay now');
     try {
