be90b285d4d4f969991d9eac51ebe9b7fcc437b4
Ed Graham <ed.graham@hmcts.net>
2025-11-07 17:57:46 +0000

    Revert "Resolving merge conflicts"
    This reverts commit 904978f9e02c9c612bb4f4823379402ff26af2a7.

--- e2e/features/stepDefinitions/service/start-appeal.steps.ts
+++ e2e/features/stepDefinitions/service/start-appeal.steps.ts
@@ -45,8 +45,7 @@ Given("I complete the `Home Office details ooc` page", async function () {
   expect(await ccdFormPage.headingContains("Home Office reference number")).to.equal(
     true
   );
-//  await startAppealFlow.completeHomeOfficeReference(true, true); - this used to include the Decision Date (OOC = true)
-  await startAppealFlow.completeHomeOfficeReference(true);
+  await startAppealFlow.completeHomeOfficeReference(true, true);
 });
 
 Given("I complete the `Basic details` form", async function () {
