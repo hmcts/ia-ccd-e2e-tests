b1ee173762249cf4324572877ac58cc6dde271a3
nilay <nilay.patel@solirius.com>
2026-07-22 10:22:05 +0100

    moving tests into a nightly directory for ease of finding

--- e2e/flows/request-respondent-evidence.flow.ts
+++ e2e/flows/request-respondent-evidence.flow.ts
@@ -4,12 +4,17 @@ export class RequestRespondentEvidenceFlow {
   private ccdFormPage = new CcdFormPage();
 
   async requestRespondentEvidence(clickContinue = false) {
-    await this.ccdFormPage.selectNextStep('Request respondent evidence');
+    await this.ccdFormPage.selectNextStep('Complete case review');
+    await this.ccdFormPage.headingContains('Complete case review');
+    let currentUrl = await this.ccdFormPage.getCurrentUrl();
+    await this.ccdFormPage.click('Submit');
+    await this.ccdFormPage.waitForConfirmationScreenAndContinue(currentUrl);
 
+    await this.ccdFormPage.selectNextStep('Request respondent evidence');
     await this.ccdFormPage.headingContains('Request respondent evidence');
     await this.ccdFormPage.click('Continue');
     await this.ccdFormPage.contentContains('Check your answers');
-    const currentUrl = await this.ccdFormPage.getCurrentUrl();
+    currentUrl = await this.ccdFormPage.getCurrentUrl();
     await this.ccdFormPage.click('Send direction');
 
     if (clickContinue) {
